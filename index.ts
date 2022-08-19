ee83285a7b1cd32c8cc22b3c5f93fa45be260672
Sayan Biswas <sayan@pokurt.me>
2022-08-19 20:26:01 +0000

    add dummy data and getshops route
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import { sequelizeClient } from "./database/client";
 import { port } from "./config.json"
 import { addNewUser, getUser } from "./routes/user";
 import { updateLocation } from "./routes/loc";
+import { getShops } from "./routes/getshops";
 
 const app = express();
 app.use(cors());
@@ -17,6 +18,7 @@ app.use(bodyParser.json())
 app.post('/adduser', addNewUser)
 app.post('/getuser', getUser)
 app.post('/updatelocation', updateLocation)
+app.get('/getshops', getShops)
 
 sequelizeClient.sync().then(() => {
     console.log("Database synced");
