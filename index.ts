7a8d2ae624a761ec911ad5a250139de60e47ec4d
Sayan Biswas <sayan@pokurt.me>
2022-08-22 13:54:58 +0000

    fix cors issues

--- index.ts
+++ index.ts
@@ -9,7 +9,23 @@ import { getShops } from "./routes/getshops";
 import { getallOrders, getMatchingOrder, getOrder, makeOrder, updateDeliveryPerson } from "./routes/orders";
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+	
+
+app.all("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  return next();
+});
+
+app.all("/*", function(req, res, next) {
+  if (req.method.toLowerCase() !== "options") {
+    return next();
+  }
+  return res.send(204);
+});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
